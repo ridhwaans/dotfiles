8beac15919b522b79c430f010eeb6dde2e05dbd8
Rid <shakeel.ridhwaan@gmail.com>
2017-10-16 13:31:49 -0400

    Update setup wishabi and corp_ad_ctc

--- setup_corp_ad_ctc.sh
+++ setup_corp_ad_ctc.sh
@@ -2,6 +2,26 @@
 
 # Get Canadian Tire projects
 projects=(
+	adaptive-model
+	admin-api
+	admin-tools
+	data-lib
+	data-loader
+	data-processing
+	ingestion
+	knowledge-graph
+	lemma-plugin
+	metrics
+	nlp
+	pipeline-core
+	rules-engine
+	search
+	search-api
+	search-core
+	search-proxy
+	search-web
+	spin-importer
+	storage
 )
 
 for project in "${projects[@]}"
